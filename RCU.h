7d4dac77ff08f691ff9284fdb0b6dc315c80241b
staticxrjc <staticxrjc@gmail.com>
2022-12-28 18:14:49 -0700

    Adding CMAKE and State Machine

--- RCU.h
+++ RCU.h
@@ -2,6 +2,12 @@
 #define RCU_H
 
 #include <iostream>
+// #ifdef THREADPOOL
 #include "Threadpool/ThreadpoolManager.h"
+// #endif
+
+// #ifdef SFML_UTIL
+#include "StateMachine/StateMachine.h"
+//#endif
 
 #endif // RCU_H
---
+++ StateMachine/StateMachine.h
@@ -0,0 +1,42 @@
+#ifndef STATEMACHINE_H
+#define STATEMACHINE_H
+
+#include <memory>
+#include <stack>
+
+namespace RCU {
+
+typedef std::unique_ptr<State> StateRef;
+
+struct State {
+    virtual void Init () = 0;
+    virtual void HandleInput () = 0;
+    virtual void Update (float dt) = 0;
+    virtual void Draw (float dt) = 0;
+    virtual void Pause ();
+    virtual void Resume ();
+};
+
+class StateMachine {
+public:
+    StateMachine() {};
+    ~StateMachine() {};
+
+public: 
+    void AddState(StateRef newState, bool isReplacing = true);
+    void RemoveState();
+    void ProcessStateChanges();
+    StateRef& GetActiveState();
+
+private:
+    std::stack<StateRef> _states;
+    StateRef _newState;
+
+    bool _isRemoving;
+    bool _isAdding;
+    bool _isReplacing;
+};
+
+}
+
+#endif // STATEMACHINE_H
