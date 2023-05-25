9cafecf64a9f14f6831fe4410786249fe695d1e9
staticxrjc <staticxrjc@gmail.com>
2023-05-25 16:43:02 -0700

    Working JSon creation and printing of JSON

--- Parsers/JSON/JSONValue.cpp
+++ Parsers/JSON/JSONValue.cpp
@@ -11,6 +11,12 @@ namespace RCU
     // JSONValue<T>::JSONValue(T val) : type(Type::Number), value(val) {}
 
     template<> 
+    RCU::JSONValue<jObject>::JSONValue() : type(JSON::Type::Object), value() {}
+
+    template<>
+    RCU::JSONValue<jArray>::JSONValue() : type(JSON::Type::Array), value() {}
+
+    template<> 
     RCU::JSONValue<std::string>::JSONValue() : type(JSON::Type::String), value("") {}
 
     template<> 
@@ -39,11 +45,17 @@ namespace RCU
 
 // Constructors
 template JSONValue<int>::JSONValue();
+template JSONValue<bool>::JSONValue();
 template JSONValue<std::string>::JSONValue(std::string val);
 template JSONValue<std::string>::JSONValue();
+template JSONValue<jObject>::JSONValue();
+template JSONValue<jArray>::JSONValue();
 
 // Destructors
 template JSONValue<int>::~JSONValue();
+template JSONValue<bool>::~JSONValue();
+template JSONValue<jObject>::~JSONValue();
+template JSONValue<jArray>::~JSONValue();
 template JSONValue<std::string>::~JSONValue();
 
 } // namespace RCU
--- Parsers/JSON/JSONValue.h
+++ Parsers/JSON/JSONValue.h
@@ -1,19 +1,19 @@
 #ifndef JSONVALUE_H
 #define JSONVALUE_H
 #include <unordered_map>
+#include <iostream>
 #include <memory>
+#include <string>
 
 namespace RCU
 {
 
 template<typename T>
 struct JSONValue;
-struct JSONContainer;
+struct JSONContainerBase;
 
-using JSONObject = std::unordered_map<std::string,std::unique_ptr<JSONContainer>>;
-
-// template<typename T>
-// using JSONArray = std::vector<std::unique_ptr<JSONValue<T>>>;
+using jObject = std::unordered_map<std::string,std::unique_ptr<JSONContainerBase>>;
+using jArray = std::vector<std::unique_ptr<JSONContainerBase>>;
 
 struct JSON {
     enum class Type {
@@ -36,30 +36,97 @@ public:
     JSONValue();
     JSONValue(T val);
     // JSONValue(const T& val);
-    // JSONValue(const JSONObject<T>& val) : type(Type::Object), value(val) {}
-    // JSONValue(const JSONArray<T>& val) : type(Type::Array), value(val) {}
     ~JSONValue();
 };
 
-struct JSONContainer {
+struct JSONContainerBase {
+    int tab = 2;
     virtual void setValue(std::string val) {}
     virtual void setValue(int val) {}
-    virtual std::string getString(){ return ""; }
-    virtual int getNumber(){ return 0; }
+    virtual void setValue(bool) {}
+    virtual std::string getString() = 0;
+    virtual int getNumber() = 0;
+    virtual bool getBool() = 0;
+    virtual jObject& getObject() { jObject nil; return nil; }
+    virtual jArray& getArray() { jArray nil; return nil; }
+    virtual void printSelf(int level = 0) {};
+    void printSpaces(int spaces) { for(int i = 0; i < (spaces * tab); i++) { std::cout << " ";}}
 };
 
-struct JSONNumber : JSONContainer {
-    JSONValue<int> Json;
+template<typename T>
+struct JSONContainer : JSONContainerBase, JSONValue<T> {
+    JSONValue<T> Json;
+};
+
+struct JSONNumber : JSONContainer<int> {
     JSONNumber(int val = 0) { Json.value = val; }
     void setValue(int val) override { Json.value = val; }
     int getNumber() override { return Json.value; }
+    std::string getString() override { return std::to_string(Json.value); }
+    bool getBool() override { return (Json.value == 0) ? false : true; }
+    void printSelf(int level = 0) override { 
+        std::cout << getString() << std::endl;
+    }
 };
 
-struct JSONString : JSONContainer {
-    JSONValue<std::string> Json;
+struct JSONString : JSONContainer<std::string> {
     JSONString(std::string val = "") { Json.value = val; }
     void setValue(std::string val) override { Json.value = val; }
+    int getNumber() override { return std::stoi(Json.value); }
     std::string getString() override { return Json.value; }
+    bool getBool() override { return (Json.value == "true") ? true : false; }
+    void printSelf(int level = 0) override { 
+        std::cout << "\"" << getString() << "\"\n";
+    }
+};
+
+struct JSONBool : JSONContainer<bool> {
+    JSONBool(bool val = false) { Json.value = val; }
+    void setValue(bool val) override { Json.value = val; }
+    int getNumber() override { return Json.value ? 1 : 0; }
+    std::string getString() override { return Json.value ? "true" : "false"; }
+    bool getBool() override { return Json.value; }
+    void printSelf(int level = 0) override { 
+        std::cout << getString() << std::endl;
+    }
+};
+
+struct JSONObject : JSONContainer<jObject> {
+    int getNumber() override { return 0; }
+    std::string getString() override { return ""; }
+    bool getBool() override { return true; }
+    jObject& getObject() override { return Json.value; }
+    void printSelf(int level = 0) override { 
+        std::cout << "\n";
+        printSpaces(level);
+        std::cout << "{\n";
+        for(auto& it : Json.value) {
+            printSpaces(level);
+            std::cout << "  \"" << it.first << "\" : ";
+            it.second->printSelf(level + 1);
+        }
+        printSpaces(level);
+        std::cout << "}\n";
+    }
+};
+
+struct JSONArray : JSONContainer<jArray> {
+    int getNumber() override { return 0; }
+    std::string getString() override { return ""; }
+    bool getBool() override { return true; }
+    jArray& getArray() override { return Json.value; }
+    void printSelf(int level = 0) override { 
+        std::cout << "[";
+        for(int i = 0; i < Json.value.size(); i++) {
+            Json.value[i]->printSelf(level);
+            if(i != (Json.value.size()-1)) {
+                printSpaces(level);
+                std::cout << ",";
+            }
+        }
+        printSpaces(level);
+        std::cout << "]\n";
+    }
 };
     
 } // namespace RCU
